c2023114ca993d40b6057b941871bc6554df3f5a
junolu <juno_250@live.com>
2023-04-17 23:26:02 +0800

    update

--- leaf-server/src/main/java/com/sankuai/inf/leaf/server/Constants.java
+++ leaf-server/src/main/java/com/sankuai/inf/leaf/server/Constants.java
@@ -5,6 +5,7 @@ public class Constants {
     public static final String LEAF_JDBC_URL = "leaf.jdbc.url";
     public static final String LEAF_JDBC_USERNAME = "leaf.jdbc.username";
     public static final String LEAF_JDBC_PASSWORD = "leaf.jdbc.password";
+    public static final String LEAF_JDBC_DRIVER = "leaf.jdbc.driver-class-name";
 
     public static final String LEAF_SNOWFLAKE_ENABLE = "leaf.snowflake.enable";
     public static final String LEAF_SNOWFLAKE_PORT = "leaf.snowflake.port";
